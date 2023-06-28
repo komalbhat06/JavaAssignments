5891ad33e5be294926a7fe6e77d981fab4611698
Komal Bhat <68904706+komalbhat06@users.noreply.github.com>
2023-06-28 11:33:13 +0530

    Update PersonApp.java

--- person/PersonApp.java
+++ person/PersonApp.java
@@ -1,12 +1,25 @@
 package com.example;
 
-public class PersonApp{
+public class PersonApp {
     public static void main(String[] args) {
-        Politician politician = new Politician("John Doe", 45, 10);
-        politician.displayInfo();
+        Person person1 = new Politician("Sidhu", 45, 10);
+        displayPersonInfo(person1);
 
-        Sportsman sportsman = new Sportsman("Jane Smith", 30, "Tennis");
-        sportsman.displayInfo();
+        Person person2 = new Sportsman("Shuru", 30, "Cricket");
+        displayPersonInfo(person2);
+    }
+
+    public static void displayPersonInfo(Person person) {
+        person.displayInfo();
+
+        if (person instanceof Politician) {
+            System.out.println("Person is a Politician");
+        } else if (person instanceof Sportsman) {
+            System.out.println("Person is a Sportsman");
+        }
+
+        System.out.println();
     }
 }
+
 
